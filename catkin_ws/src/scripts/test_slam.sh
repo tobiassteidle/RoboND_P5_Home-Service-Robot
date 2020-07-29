d2dccd14aaac943530b988ef968207bbfa3d79c9
Tobias Steidle <tobias.steidle@softwaredev.de>
2020-07-28 23:11:47 -0700

    feat: added script for slam test (test_slam.sh)

---
+++ catkin_ws/src/scripts/test_slam.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch  " &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch" &
